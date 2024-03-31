7121222d8c49c193d71f5fbc8b9b4bbc2d0e7d72
Anthony Camarillo <anthony.camarillo.96@gmail.com>
2024-03-31 11:28:47 -0700

    (feat): set up the blog, now I have to write

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,12 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 
-const blogCollection = defineCollection({});
+const blogCollection = defineCollection({
+  'type': 'content',
+  schema: z.object({
+    title: z.string(),
+    category: z.string(),
+  })
+});
 
 export const collections = {
   'blog': blogCollection,
